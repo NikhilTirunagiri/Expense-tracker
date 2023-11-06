fe857b764e6837e3015c55bb81308f94d6e16484
NikhilTirunagiri <nikhiltirunagiri@outlook.com>
2023-11-06 11:51:29 +0530

    skeletion

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>$400<span>.00</span></h1>
+      <form>      
+
+        <div className='basic'>
+          <input type='text' placeholder={'+200 new TV'}/>
+          <input type='datetime-local'/>
+        </div>
+
+        <div>
+        <input type='text' placeholder={'description'}/>
+        </div>
+        <button type="submit">Add new transaction</button>
+      </form>
+
+      <div className='transactions'>
+        <div className='transaction'>
+          <div className='left'>
+            <div className='name'>new samsung tv</div>
+            <div className='description'>old tv is broken</div>
+          </div>
+          <div className='right'>
+            <div className='price'>$500</div>
+            <div className='datetime'>2023-11-06 11:47</div>
+          </div>
+
+        </div>
+        <div className='transaction'>
+          <div className='left'>
+            <div className='name'>iphone</div>
+            <div className='description'>old iphone is broken</div>
+          </div>
+          <div className='right'>
+            <div className='price'>$1000</div>
+            <div className='datetime'>2023-11-05 10:47</div>
+          </div>
+
+        <div className='transaction'>
+          <div className='left'>
+            <div className='name'>new laptop</div>
+            <div className='description'>old laptop is broken</div>
+          </div>
+          <div className='right'>
+            <div className='price'>$1500</div>
+            <div className='datetime'>2023-11-03 09:47</div>
+          </div>
+
+        </div>
+        </div>
+      </div>
+
+    </main>
   );
 }
 
